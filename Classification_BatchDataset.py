54b4019dec424f67d9a68dc7efd329599afb3819
Your Name <you@example.com>
2017-12-18 19:37:16 +0100

    init

---
+++ Classification_BatchDataset.py
@@ -0,0 +1,84 @@
+"""
+Code ideas from https://github.com/Newmu/dcgan and tensorflow mnist dataset reader
+"""
+import numpy as np
+import scipy.misc as misc
+
+
+class Classification_BatchDataset:
+    path = ""
+    class_mappings = ""
+    files = []
+    images = []
+    annotations = []
+    batch_offset = 0
+    epochs_completed = 0
+
+    def __init__(self, records_list):
+        """
+        Initialize a file reader for the DeepScores classification data
+        :param records_list: path to the dataset
+        sample record: {'image': f, 'annotation': annotation_file, 'filename': filename}
+        """
+        print("Initializing DeepScores Classification Batch Dataset Reader...")
+        self.path = records_list
+
+
+        self._read_images()
+
+    def _read_images(self):
+        self.__channels = True
+        self.images = np.array([self._transform(filename['image']) for filename in self.files])
+        self.__channels = False
+        self.annotations = np.array(
+            [np.expand_dims(self._transform(filename['annotation']), axis=3) for filename in self.files])
+        print (self.images.shape)
+        print (self.annotations.shape)
+
+    def _transform(self, filename):
+        image = misc.imread(filename)
+        if self.__channels and len(image.shape) < 3:  # make sure images are of shape(h,w,3)
+            image = np.array([image for i in range(3)])
+
+        if self.image_options.get("resize", False) and self.image_options["resize"]:
+            resize_size = int(self.image_options["resize_size"])
+            resize_image = misc.imresize(image,
+                                         [resize_size, resize_size], interp='nearest')
+        else:
+            resize_image = image
+
+        return np.array(resize_image)
+
+    def get_records(self):
+        return self.images, self.annotations
+
+    def reset_batch_offset(self, offset=0):
+        self.batch_offset = offset
+
+    def next_batch(self, batch_size):
+        start = self.batch_offset
+        self.batch_offset += batch_size
+        if self.batch_offset > self.images.shape[0]:
+            # Finished epoch
+            self.epochs_completed += 1
+            print("****************** Epochs completed: " + str(self.epochs_completed) + "******************")
+            # Shuffle the data
+            perm = np.arange(self.images.shape[0])
+            np.random.shuffle(perm)
+            self.images = self.images[perm]
+            self.annotations = self.annotations[perm]
+            # Start next epoch
+            start = 0
+            self.batch_offset = batch_size
+
+        end = self.batch_offset
+        return self.images[start:end], self.annotations[start:end]
+
+    def get_random_batch(self, batch_size):
+        indexes = np.random.randint(0, self.images.shape[0], size=[batch_size]).tolist()
+        return self.images[indexes], self.annotations[indexes]
+
+
+
+if __name__ == "__main__":
+    data_reader = Classification_BatchDataset("../Datasets/Deepscores/classification_data")
